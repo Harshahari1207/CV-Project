2d791df7e1cb07d987603f1767bcf89ed645c49a
HARSHAKUMAR REDDY BALIREDDYGARI <102666296+Harshahari1207@users.noreply.github.com>
2023-03-25 22:08:13 +0530

    Default Education Information

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,9 +1,18 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-class Education extends Component{
-    render(){
-        return <h3>Education</h3>
-    }
+class Education extends Component {
+  render() {
+    return (
+      <div className="education">
+        <h3>Education</h3>
+        <h4>Bachelores of Technology</h4>
+        <ul>
+          <li>Birla Institute of Technology, Mesra(2022 Completed)</li>
+          <li>Electronics and Communication Engineering</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
-export {Education};
+export { Education };
