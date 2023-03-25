e6b77c96ddd9c107614fff3ab04f1ac2e7bd136a
HARSHAKUMAR REDDY BALIREDDYGARI <102666296+Harshahari1207@users.noreply.github.com>
2023-03-25 21:55:36 +0530

    Default Projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,20 @@ import React, { Component } from "react";
 
 class Projects extends Component {
   render() {
-    return <h3>Projects</h3>;
+    return (
+      <div className="projects">
+        <h3>Projects</h3>
+        <div className="projectsContents">
+          <h4>Tic Tac Toe Web App</h4>
+          <p>
+            The Tic Tac Toe Web App is a game for two players, called "X" and
+            "O", who take turns marking the spaces in a 3x3 grid. The player who
+            succeeded in placing three respective marks in a horizontal,
+            vertical, or diagonal row wins the game. 
+          </p>
+        </div>
+      </div>
+    );
   }
 }
 
