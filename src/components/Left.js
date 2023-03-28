d30ce036fa1e42753368babf3c429c5f17a4002a
HARSHAKUMAR REDDY BALIREDDYGARI <102666296+Harshahari1207@users.noreply.github.com>
2023-03-28 20:21:50 +0530

    Profile photo.

--- src/components/Left.js
+++ src/components/Left.js
@@ -2,19 +2,23 @@ import React, { Component } from "react";
 import { Contact } from "./Contact";
 import { Skills } from "./Skills";
 import { Interests } from "./Interests";
+import img from "../img/myImage.jpg";
 
 class Left extends Component {
-  //   constructor() {
-  //     super();
-  //     this.state = {};
-  //   }
+    constructor() {
+      super();
+      this.state = {
+        imgLoad: img,
+      };
+      // this.imgChanger =  onClick=this.imgChanger 
+    }
 
   render() {
     return (
       <div className="left">
         
         <div className="profileImg">
-          <h2>Image</h2>
+          <img src={this.state.imgLoad}/>
         </div>
         <Contact />
         <Skills />
